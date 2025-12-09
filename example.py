ef66c3ad10b1a5a7ad56708728293edf1dbcd9f3
Monsta <monsta8896@gmail.com>
2025-12-09 19:59:48 +0530

    Matching example.py to README.md

--- example.py
+++ example.py
@@ -1,26 +1,28 @@
 import pytz
+import builtins
 from printlogger import Config, Print
 
-# 1. Define Configuration
-config = Config(
-    app_name="MyBot",
-    logs_dir="logs",
-    retention_days=30,
-    timezone=pytz.timezone("America/New_York"),
-    use_console_colors=True,
+builtins.print = Print(Config(
+    filename_fmt = "%B_%d-%y.log",
+    
+    tag_info = "[i]",
+    tag_error = "[e]",
+    tag_warning = "[w]",
+    tag_success = "[s]",
+    tag_debug = "[d]",
+    tag_critical = "[c]",
+    
     colors={
-        "info": "\033[37m",     # White
-        "error": "\033[91m",    # Light Red
-        "success": "\033[92m",  # Light Green
-        "warning": "\033[93m",  # Light Yellow
-        "debug": "\033[90m",    # Dark Gray
-        "critical": "\033[41m", # Red Background
-        "reset": "\033[0m"      # Reset
+        "normal": "\033[37m",
+        "info": "\033[34m",
+        "error": "\033[31m",
+        "warning": "\033[33m",
+        "success": "\033[32m",
+        "debug": "\033[36m",
+        "critical": "\033[41m\033[37m",
+        "reset": "\033[0m"
     }
-)
-
-# 2. Create the Logger
-print = Print(config)
+))
 
 # Standard usage (Defaults to INFO level)
 print("System initializing...")
@@ -28,6 +30,7 @@ print("Loading modules", end="...")
 print("Done!")  # Appends to the same line in file/console
 
 # Specific Log Levels
+print.info("Connecting to database...")
 print.success("Database connected successfully.")
 print.warning("High latency detected:", "450ms")
 print.error("Connection dropped.")
